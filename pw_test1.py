dab99ccf8b13e07ee104c5fbc284eeb13576c3fa
Raz <razlintt@gmail.com>
2024-10-31 16:32:23 +0800

    Added bs4 simple parsing

--- pw_test1.py
+++ pw_test1.py
@@ -1,11 +1,14 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
-
+from bs4 import BeautifulSoup
+import re
+import time
 
 def run(playwright: Playwright) -> None:
+    websiteLink = "https://www.carousell.sg/"
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://www.carousell.sg/")
+    page.goto(websiteLink)
     page.get_by_placeholder("Search for an item").click()
     page.get_by_placeholder("Search for an item").fill("y15")
     page.get_by_role("button", name="y15 in Class 2B").click()
@@ -21,9 +24,18 @@ def run(playwright: Playwright) -> None:
     html_content = page.content()
 
     # Save the HTML content to a text file
-    with open("page_content.txt", "w", encoding="utf-8") as file:
+    with open("page_content_y15.txt", "w", encoding="utf-8") as file:
         file.write(html_content)
         
+    soup = BeautifulSoup(html_content, "html.parser")
+
+    links = soup.find_all("a")
+    for followUp in links:
+        href = followUp.get("href")
+        link = websiteLink[:-1] + href
+
+        print(link)
+
     # ---------------------
     context.close()
     browser.close()
